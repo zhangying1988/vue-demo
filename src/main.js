ebe12ffeb16af1bda070496d614cf200204639b6
zhangying <1019797255@qq.com>
2020-03-12 21:42:47 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from "@/router/router";
+import fastclick from 'fastclick'//300毫秒延迟点击问题’
+import './assets/styles/reset.css' //引入全局样式
+import './assets/styles/border.css'//一像素边框border.css
+Vue.config.productionTip = false
+fastclick.attach(document.body)//使用fastclick
+new Vue({
+    router,
+    render: h => h(App),
+}).$mount('#app')
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div>home</div>
+        <router-link to="./List">列表</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
