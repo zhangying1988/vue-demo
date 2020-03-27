2c70159ee609bf98f483cb2cf6aba9fbae109b8c
yingzhang <1019797255@qq.com>
2020-03-27 10:21:42 +0800

    'tijiao'

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from "@/router/router";
+import router from './router/router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import fastclick from 'fastclick'//300毫秒延迟点击问题’
 import './assets/styles/reset.css' //引入全局样式
 import './assets/styles/border.css'//一像素边框border.css
 import './assets/styles/iconfont.css'//icon
+import 'swiper/dist/css/swiper.css'
+
 Vue.config.productionTip = false
 fastclick.attach(document.body)//使用fastclick
+Vue.use(VueAwesomeSwiper)
 new Vue({
     router,
     render: h => h(App),
